00929dc0c1fb4e9847c818e7bc23aebb6642b7bd
lealbaugh <l.e.albaugh@gmail.com>
2013-11-05 12:42:10 -0500

    Making curl-ready

--- cocktails.py
+++ cocktails.py
@@ -5,10 +5,9 @@ import os
 from pymongo import *
 
 debug = False
+i = 0
 
 app = Flask(__name__)
-
-i = 0
 
 # Twilio account info, to be gotten from Heroku environment variables
 account_sid = os.environ['ACCOUNT_SID'] 
@@ -22,7 +21,6 @@ databasename = mongoclientURL.split("/")[-1] #gets the last bit of the URL, whic
 
 client = MongoClient(mongoclientURL)
 database = client[databasename]	#loads the assigned database
-
 collection = database["phonenumber"] #loads or makes the collection, whichever should happen
 
 
@@ -35,20 +33,19 @@ def index():
 def handle_form():
 	sendtonumber = request.form.get('From', None)
 	content = request.form.get('Body', "empty text?")
-	newcontentobject = {"position": i, "content": content}
+	
+	collection.insert({"position":i, "content":content})
 	i = i+1
-	collection.insert(newcontentobject)
 
-
-	print sendtonumber+"\n"
-	print twilionumber
 	try:
 		client = TwilioRestClient(account_sid, auth_token)
 		message = client.sms.messages.create(body="sent from python!", to=sendtonumber, from_=twilionumber)
  	except twilio.TwilioRestException as e:
  		print e
+ 		return e
 
-	return render_template("template.html", information = collection)
+	return sendtonumber+"\n"+content
+	# render_template("template.html", information = collection)
 
 if __name__ == "__main__":
 	app.run(debug=debug)
