92eed470c9110ea7168a7c3c1d7898abb1448254
lealbaugh <l.e.albaugh@gmail.com>
2013-11-06 18:19:17 -0500

    Applying what we've learned to a subtly different case.

--- cocktails.py
+++ cocktails.py
@@ -82,9 +82,9 @@ def newPlayer(phonenumber, content):
 def getAgentName(phonenumber, content):
 	# players.find for player, based on phone number
 	# return player agent name
-	agentname = lookup(collection=players, field="phonenumber", fieldvalue=phonenumber, response="agentname")
-	if agentname is None:
+	if players.find({"agentname": agentname}).count() = 0:
 		agentname = newPlayer(phonenumber, content)
+	agentname = lookup(collection=players, field="phonenumber", fieldvalue=phonenumber, response="agentname")
 	return agentname
 
 
