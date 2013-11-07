412f6f573df9682af6910eb1661ac16fa9a387a1
lealbaugh <l.e.albaugh@gmail.com>
2013-11-06 22:37:23 -0500

    More debug!

--- cocktails.py
+++ cocktails.py
@@ -101,7 +101,7 @@ def gameLogic(agentname, content):
 	print "gamelogic!"
 	recipient = re.match("\d{3,4}", content).group(0)
 	if recipient:
-		print "direct message"
+		print "direct message to"+recipient
 		content = re.sub("\d{3,4}", "From "+agentname, content)
 		sendToRecipient(content = content, recipient = recipient, sender = agentname)
 		print "sent "+content+" to "+recipient
